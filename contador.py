db55af3e096c5768c2ed17bd3c50c59480f64515
calel95 <64104649+calel95@users.noreply.github.com>
2021-09-28 20:03:38 -0300

    Update contador.py

--- contador.py
+++ contador.py
@@ -4,4 +4,6 @@ if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
     words = sc.textFile("gs://{SEU_BUCKET}/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    wordCounts.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
+    for i in range(10):    
+      wd2 = wordCounts[i]
+      wd2.saveAsTextFile("gs://{SEU_BUCKET}/resultado")
